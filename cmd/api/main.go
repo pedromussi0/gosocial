038547732d0d5dd7cf8b7d15011964fca5a18f07
Pedro Mussi <phpmussi@gmail.com>
2024-12-21 14:20:50 -0300

    setting up hot reload and env vars

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg := config{
-		addr: ":8080",
+		addr: os.Getenv("ADDR"),
 	}
 
 	app := &application{
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/pedromussi0/gosocial.git
 go 1.21
 
 require github.com/go-chi/chi/v5 v5.2.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
