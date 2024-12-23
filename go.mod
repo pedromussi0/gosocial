7ef747148294e52d122e91db87f7e26debfa3733
Pedro Mussi <phpmussi@gmail.com>
2024-12-23 11:33:55 -0300

    initializing data persistence

--- go.mod
+++ go.mod
@@ -4,4 +4,6 @@ go 1.21
 
 require github.com/go-chi/chi/v5 v5.2.0
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
+
+require github.com/lib/pq v1.10.9 // indirect
--- internal/store/users.go
+++ internal/store/users.go
@@ -1,6 +1,49 @@
-package main
+package store
+
+import (
+	"context"
+	"database/sql"
+)
 
 type User struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	//	Password  password `json:"-"`
+	CreatedAt string `json:"created_at"`
+	IsActive  bool   `json:"is_active"`
+	//RoleID    int64    `json:"role_id"`
+	//Role      Role     `json:"role"`
+}
+
+// type password struct {
+// 	text *string
+// 	hash []byte
+// }
+
+type UsersStore struct {
+	db *sql.DB
+}
+
+func (s *UsersStore) Create(ctx context.Context, user *User) error {
+	query := `
+	INSERT INTO users (username, password, email)
+	VALUES ($1, $2, $3, $4) RETURNING id, created_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		//user.Password,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
