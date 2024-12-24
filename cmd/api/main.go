87c6afb3eff7c9f049d1ac9c09c8d68744ed0c9b
Pedro Mussi <phpmussi@gmail.com>
2024-12-24 10:13:34 -0300

    setting up db config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/joho/godotenv"
 
+	"github.com/pedromussi0/gosocial.git/internal/db"
 	"github.com/pedromussi0/gosocial.git/internal/store"
 )
 
@@ -17,9 +18,25 @@ func main() {
 
 	cfg := config{
 		addr: os.Getenv("ADDR"),
+		db: dbConfig{
+			addr:         os.Getenv("DB_ADDR"),
+			maxOpenConns: 30,
+			maxIdleConns: 30,
+			maxIdleTime:  "15m",
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
