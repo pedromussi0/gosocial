0f80f4fb3caee00818b59ddd405d8cfa73f2058e
Pedro Mussi <phpmussi@gmail.com>
2025-01-11 13:36:28 -0300

    feat: pagination set up in feed endpoint

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/pedromussi0/gosocial.git/internal/store"
 )
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
+
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequest(w, r, err)
+		return
+	}
+
 	ctx := r.Context()
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(95))
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(95), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
